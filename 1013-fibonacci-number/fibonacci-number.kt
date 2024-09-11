b787ab5a62fdbf61bf0e71f5d7d1cae826302bcb
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2024-09-11 07:31:57 +0530

    Time: 119 ms (80.49%) | Memory: 33.2 MB (32.62%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun fib(n: Int): Int {
+        if(n==0) return 0
+        if(n==1) return 1
+        var a = 0
+        var b = 1
+        for(i in 2..n){
+            val temp = a + b
+            a = b
+            b = temp 
+
+
+        }
+        return b
+    }
+}
