58c7f25d03feac41acee5803dd9eb26a29be88d0
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2024-09-18 21:52:19 +0530

    Time: 172 ms (10.91%) | Memory: 33.4 MB (90.23%) - LeetSync

--- 69-sqrtx/sqrtx.kt
+++ 69-sqrtx/sqrtx.kt
@@ -1,15 +1,17 @@
 class Solution {
     fun mySqrt(x: Int): Int {
+        if (x == 0) return 0  // Handle the case where x is 0
+
         var left = 1
         var right = x
-            while (left<=right){
-                val middle = ( left + right )  / 2
-                if (x / middle == middle){
-                    return middle
-                }else if (x / middle<middle){
-                  right = middle-1
-                 }else{
-                      left=middle+1
+        while (left <= right) {
+            val middle = left + (right - left) / 2
+            if (middle == x / middle) {
+                return middle
+            } else if (middle > x / middle) {
+                right = middle - 1
+            } else {
+                left = middle + 1
             }
         }
         return right
