5ec7e29894a4159f7aa9a125b5f8b07ec8e5a451
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2024-09-27 21:36:47 +0530

    Time: 210 ms (73.94%) | Memory: 36.5 MB (81.45%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun isPalindrome(x: Int): Boolean {
+         if(x < 0 )
+        return false 
+
+        var num = x
+        var rev = 0
+
+        while(num != 0){
+            var pop = num % 10
+            num = num / 10
+            rev = rev * 10 + pop
+        }
+
+
+        if(x == rev)
+        return true 
+        else 
+        return false
+    }
+}
