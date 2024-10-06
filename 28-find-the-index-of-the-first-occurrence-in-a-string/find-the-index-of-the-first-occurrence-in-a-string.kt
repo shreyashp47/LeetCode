995d9b435e74b1fc1b878da7451276d8e30eb990
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2024-10-06 22:33:14 +0530

    Time: 176 ms (8.71%) | Memory: 34.9 MB (36.44%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.kt
@@ -0,0 +1,17 @@
+class Solution {
+     fun strStr(haystack: String, needle: String): Int {
+      
+       if(needle.length > haystack.length)
+      return -1
+      
+     for(i in 0..(haystack.length - needle.length)){
+         println(i)
+         if(haystack.substring(i,i+needle.length) == needle){
+             return i
+         }
+     }
+      
+     
+        return -1
+        }
+}
