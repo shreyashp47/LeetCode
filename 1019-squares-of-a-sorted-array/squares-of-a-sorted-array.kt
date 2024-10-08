ed245f78b76bf066b6666ca446243fb642e8ab42
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2024-10-08 07:20:32 +0530

    Time: 722 ms (5.41%) | Memory: 42 MB (39.50%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.kt
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.kt
@@ -1,18 +1,31 @@
 class Solution {
-    fun sortedSquares(nums: IntArray): IntArray {
+      fun sortedSquares(nums: IntArray): IntArray {
         
-        println("size ${nums.size}")
-        var ans = IntArray(nums.size)
-        
-        for(i in 0..nums.size-1){
-            println(nums[i]*nums[i])
-            ans[i] = nums[i] * nums[i]
-            println("ans[i] ${ans[i]}")
-        }
-        ans.sort()
+        var result = IntArray(nums.size)
+        var left = 0
+        var pos = nums.size-1
+        var right = nums.size-1
         
         
-        return ans
+       while(left <= right){
+          	val l = nums[left] * nums[left] 
+         	val  r = nums[right] * nums[right] 
+           println("sqare $l $r")
+           if(l < r){ 
+               result[pos] = r
+               right--
+           }else{  
+               result[pos] = l
+               left++
+               
+           }
+           println(result[pos])
+           pos--
+           
+           
+       }
+        
+        return result
         
     }
 }
