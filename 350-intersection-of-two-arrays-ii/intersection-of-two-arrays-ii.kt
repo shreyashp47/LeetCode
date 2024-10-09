17f6418ce6dab679368a668ee3e69e857441c94d
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2024-10-09 22:24:16 +0530

    Time: 183 ms (90.64%) | Memory: 38.1 MB (45.32%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.kt
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.kt
@@ -1,12 +1,10 @@
 class Solution {
     fun intersect(nums1: IntArray, nums2: IntArray): IntArray {
-         nums1.sort()
-     nums2.sort()
-     var array = ""
-     
-     var n1 = 0
-     var n2 = 0
-     var k = 0
+        nums1.sort()
+        nums2.sort()
+        var n1 = 0
+        var n2 = 0
+        var k = 0
      
      while(n1 < nums1.size && n2 < nums2.size){ 
          if(nums1[n1] < nums2[n2]){
@@ -19,9 +17,7 @@ class Solution {
              n1++
              n2++
          }
-         
-     }
-     
+         }
      return nums1.copyOfRange(0,k)
     }
 }
