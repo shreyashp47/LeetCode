72e3d5e430d74e135f4bc2a9d8424d8d1f97ee43
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2024-10-11 22:20:24 +0530

    Time: 153 ms (8.00%) | Memory: 33.2 MB (60.00%) - LeetSync

--- 1610-xor-operation-in-an-array/xor-operation-in-an-array.kt
+++ 1610-xor-operation-in-an-array/xor-operation-in-an-array.kt
@@ -1,10 +1,8 @@
 class Solution {
     fun xorOperation(n: Int, start: Int): Int {
-
           var result = 0
     for (i in 0 until n) {
         result = result xor (start + 2 * i)
-        print(result)
     }
     return result
     }
