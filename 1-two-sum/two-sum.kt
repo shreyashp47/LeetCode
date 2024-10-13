3c7b9738d36d092e54c5caa20db91a38de97eefc
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2024-10-13 13:17:48 +0530

    Time: 202 ms (72.59%) | Memory: 37.8 MB (82.83%) - LeetSync

---
+++ 1-two-sum/two-sum.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+         var map = HashMap<Int, Int>()
+     for(i in 0..nums.size-1){ 
+         val c =  target - nums[i] 
+         if(map.containsKey(c) ){
+             return intArrayOf(map[c]!!,i)
+         }
+         map[nums[i]] = i
+     } 
+     return intArrayOf() 
+    }
+}
