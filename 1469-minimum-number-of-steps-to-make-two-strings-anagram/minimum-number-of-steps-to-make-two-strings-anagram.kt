77f61bb75aeb149da62ea2a13e8bf2e99bc43a50
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2024-10-19 13:53:06 +0530

    Time: 85 ms (100.00%) | Memory: 39.8 MB (32.00%) - LeetSync

---
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun minSteps(s: String, t: String): Int {
+    var map = HashMap<Char,Int>()
+    for(i in s.toCharArray()){
+        if(map.getOrDefault(i,0)==0)
+        map[i] = 1
+        else map[i] = map.getOrDefault(i,0)+1
+        
+    } 
+    var count = 0
+    for(i in t.toCharArray()){ 
+        if(!map.containsKey(i) ) 
+        count +=1
+        else if(map.getOrDefault(i,0)<1)
+        count +=1
+        else map[i] = map.getOrDefault(i,0)-1
+    } 
+        
+    return count
+    }
+}
