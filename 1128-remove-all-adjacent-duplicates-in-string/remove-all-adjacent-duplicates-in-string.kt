e9e5e68c69a18e973cd3d308c30d3820841f34f5
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2024-10-26 13:34:55 +0530

    Time: 54 ms (89.91%) | Memory: 39.3 MB (40.37%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun removeDuplicates(s: String): String {
+        val st = Stack<Char>()
+
+        for(i in s.toCharArray()){
+            if(st.isEmpty()){
+                st.push(i)
+            }else{
+                if(st.peek() == i){st.pop()} else  st.push(i)
+
+            }
+        
+        }
+        return st.joinToString("")
+    }
+}
