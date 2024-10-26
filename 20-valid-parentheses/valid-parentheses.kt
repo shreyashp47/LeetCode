775eb755656d394dc46bf76f2cb6fc8f9b1a4dae
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2024-10-26 13:28:43 +0530

    Time: 4 ms (98.32%) | Memory: 34.3 MB (76.09%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.kt
+++ 20-valid-parentheses/valid-parentheses.kt
@@ -1,12 +1,20 @@
 class Solution {
     fun isValid(s: String): Boolean {
-    if(s == "")
-    return true 
-    
-    val asn = s.replace("[]","").replace("()","").replace("{}","")
- 
- 	if(asn == s) return false
-    
-    return isValid(asn)
+         val stack = Stack<Char>()
+          
+          for(i in s.toCharArray()){
+            if(i == '(' || i == '[' || i == '{'){ 
+                stack.push(i)
+                }else if(i == ')' || i == ']' || i == '}'){
+                    if(stack.isEmpty()){
+                        return false
+                    }else if(i == ')' && stack.peek() == '('){ stack.pop()
+                    }else if(i == ']' && stack.peek() == '['){stack.pop()
+                    }else if(i == '}' && stack.peek() == '{'){ stack.pop()
+                    }else return false 
+                } 
+          } 
+          
+          return stack.isEmpty();
     }
 }
