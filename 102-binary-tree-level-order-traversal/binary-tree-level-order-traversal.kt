cc5b2412ddff4d37436278c3ac0eb6df0ca29f08
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2024-11-16 13:44:59 +0530

    Time: 7 ms (65.83%) | Memory: 38.5 MB (53.50%) - LeetSync

---
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.kt
@@ -0,0 +1,43 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun levelOrder(root: TreeNode?): List<List<Int>> {
+         if(root == null) return emptyList()
+
+        val result = mutableListOf<List<Int>>()
+        val queue = LinkedList<TreeNode>()
+        queue.add(root)
+
+        while(queue.isNotEmpty()){ 
+            var level = mutableListOf<Int>()
+
+            for (i in queue.indices) {
+                val node = queue.poll()
+                level.add(node.`val`)
+
+                if(node.left !=null){
+                    queue.add(node.left)
+                }
+
+                if(node.right != null){
+                    queue.add(node.right)
+                }
+
+             
+            }
+
+        result.add(level)
+        }
+       
+          
+        return result
+    }
+}
