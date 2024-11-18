fc1e2c0d631552e7432a845b96be7771a49f1098
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2024-11-18 17:44:06 +0530

    Time: 46 ms (43.90%) | Memory: 40.5 MB (7.14%) - LeetSync

---
+++ 459-repeated-substring-pattern/repeated-substring-pattern.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun repeatedSubstringPattern(s: String): Boolean {
+       var n = s.length
+     for (i in 1..n/2){
+         if(n%i == 0){
+             println(i)
+             var sub = s.substring(0,i)
+             var sb = StringBuilder()
+             repeat(n/i){
+                 sb.append(sub)
+             }
+             if(sb.toString().equals(s)) return true
+         }
+     }
+     return false
+        
+        
+    }
+}
