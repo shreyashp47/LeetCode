f616316581c610f5b62a9564296356814f03bf6c
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2024-11-23 13:07:42 +0530

    Time: 277 ms (5.47%) | Memory: 37.1 MB (5.87%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.kt
+++ 1013-fibonacci-number/fibonacci-number.kt
@@ -3,12 +3,11 @@ class Solution {
         return fibDp(n,IntArray(n + 1) { -1 })
     }
   fun fibDp(n: Int, dp: IntArray): Int {
-        if(n == 0 || n==1) return n
+    if(n == 0 || n==1) return n
     if(dp[n]!=-1) return dp[n];
     var sub1 = fib(n-1)
     var sub2 = fib(n-2)
     dp[n] = sub1 + sub2
-    return dp[n]
-        
+    return dp[n]    
     }
 }
