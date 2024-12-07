8fefb52f0a9a07ff4aa0913df1f7fbbe0557978f
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2024-12-07 13:27:24 +0530

    Time: 1 ms (61.90%) | Memory: 39.5 MB (35.29%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.kt
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.kt
@@ -2,7 +2,7 @@ class Solution {
     fun countNegatives(grid: Array<IntArray>): Int {
         var result = 0
         for(i in 0..grid.size-1){
-            for(j in 0..grid[i].size-1){
+            for(j in grid[i].size-1 downTo 0){
                 if(grid[i][j]<0)
                 result++
         }
