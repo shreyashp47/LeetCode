5ba566beefd348b856f334934218b7f23723410d
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2024-12-12 20:36:44 +0530

    Time: 0 ms (100.00%) | Memory: 35.6 MB (32.12%) - LeetSync

---
+++ 27-remove-element/remove-element.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun removeElement(nums: IntArray, `val`: Int): Int {
+       var count = 0
+        
+        for(i in 0..nums.size -1 ){ 
+            if(nums[i] != `val`){
+                nums[count] = nums[i]
+                count++
+            }
+        } 
+        return count
+    }
+}
