e5f83a5cc5546653601412803a2146037f58d3ed
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2024-12-12 14:25:44 +0530

    Time: 9 ms (90.91%) | Memory: 38 MB (33.33%) - LeetSync

---
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun pickGifts(gifts: IntArray, k: Int): Long {
+          val pq = PriorityQueue<Int>(compareByDescending { it })
+		for( i in gifts){
+    		pq.add(i)
+		}
+        for(i in 1..k){
+            var max = pq.poll()
+            var p = Math.sqrt(max.toDouble()).toInt()
+            pq.add(p)
+        }
+        var sum = 0L
+        for(i in pq){
+            sum = sum+i
+        }
+    
+    
+    return sum
+    }
+}
