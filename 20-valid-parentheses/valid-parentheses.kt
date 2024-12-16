d1615f820fec96d83ba2423c38bf19fc0f45166a
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2024-12-16 23:19:22 +0530

    Time: 5 ms (77.92%) | Memory: 35.4 MB (65.06%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.kt
+++ 20-valid-parentheses/valid-parentheses.kt
@@ -1,20 +1,19 @@
 class Solution {
     fun isValid(s: String): Boolean {
-         val stack = Stack<Char>()
-          
-          for(i in s.toCharArray()){
-            if(i == '(' || i == '[' || i == '{'){ 
+        val stack = Stack<Char>()
+        for( i  in s){
+            if(i == '(' || i== '[' || i == '{'){
                 stack.push(i)
-                }else if(i == ')' || i == ']' || i == '}'){
-                    if(stack.isEmpty()){
-                        return false
-                    }else if(i == ')' && stack.peek() == '('){ stack.pop()
-                    }else if(i == ']' && stack.peek() == '['){stack.pop()
-                    }else if(i == '}' && stack.peek() == '{'){ stack.pop()
-                    }else return false 
-                } 
-          } 
-          
-          return stack.isEmpty();
+            }
+            else if(i == ')' || i== ']' || i == '}' ){
+                if(stack.isEmpty()) return false
+                if(stack.peek() == '(' && i == ')') stack.pop()
+                else if(stack.peek() == '[' && i == ']') stack.pop()
+                else if(stack.peek() == '{' && i == '}') stack.pop()
+                else return false
+            }
+        }
+        
+        return stack.isEmpty()
     }
 }
