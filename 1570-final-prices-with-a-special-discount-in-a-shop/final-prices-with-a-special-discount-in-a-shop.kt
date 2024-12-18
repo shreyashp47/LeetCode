f3c369916f6676cbfb002744c139edc7f836321f
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2024-12-18 10:41:35 +0530

    Time: 6 ms (87.10%) | Memory: 38.3 MB (58.06%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.kt
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.kt
@@ -1,16 +1,18 @@
 class Solution {
     fun finalPrices(prices: IntArray): IntArray {
-       
+        var stack = Stack<Int>()
+        var size = prices.size-1
 
-        for(i in prices.indices){
-           
-
-            for(j in i+1..prices.size-1){
-                if(prices[i] >= prices[j]){
-                    prices[i] = prices[i] - prices[j]
-                    break
-                }
+        for(i in size downTo 0){
+            while(!stack.isEmpty() && stack.peek()>prices[i]){
+                stack.pop()
             }
+            var p = prices[i]
+            if(!stack.isEmpty()){ 
+                prices[i] =  prices[i] - stack.peek() 
+            } 
+                stack.push(p)
+            
         }
         return prices
     }
