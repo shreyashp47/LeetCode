4319a3c54d35d640be46be7ad44b7a69abde63e0
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2024-12-25 22:55:46 +0530

    Time: 24 ms (50.09%) | Memory: 39.3 MB (17.99%) - LeetSync

--- 242-valid-anagram/valid-anagram.kt
+++ 242-valid-anagram/valid-anagram.kt
@@ -1,29 +1,19 @@
 class Solution {
     fun isAnagram(s: String, t: String): Boolean {
- if(s.length != t.length){
+        if(s.length != t.length){
         return false
-     }
-    
-    var map = HashMap<Char,Int>()
-    for(i in s.toCharArray()){
-        if(map.getOrDefault(i,0)==0)
-        map[i] = 1
-        else map[i] = map.getOrDefault(i,0)+1
-        
-    }
-    println(map)
-    for(i in t.toCharArray()){
-        println("$i "+map[i])
-        println(map)
-        if(!map.containsKey(i) ) 
-        return false
-        else if(map.getOrDefault(i,0)<1)
-        return false
-        else map[i] = map.getOrDefault(i,0)-1
-    }
-    println(map)
+        }
+        var map = HashMap<Char,Int>()
+        for(i in 0..s.length-1){
+            map[s[i]] = map.getOrDefault(s[i],0) + 1
+        }
+        for(i in 0..t.length-1){
+
+            if(!map.containsKey(t[i]) || map.getOrDefault(t[i],0)==0)
+            return false
+            else map[t[i]] = map.getOrDefault(t[i],0)-1
+        }
+
         return true
     }
-
-    
 }
