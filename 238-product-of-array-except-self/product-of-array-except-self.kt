5468bd7aa2d3ec281a63c2e841c71617b10ba364
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2024-12-26 12:00:07 +0530

    Time: 322 ms (5.03%) | Memory: 49.9 MB (90.83%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.kt
+++ 238-product-of-array-except-self/product-of-array-except-self.kt
@@ -1,17 +1,17 @@
 class Solution {
     fun productExceptSelf(nums: IntArray): IntArray {
-         var n = nums.size
-       val result = IntArray(n) { 0 }
-		result[0] = 1;
-        for (i in 1..n-1) {
-            result[i] = result[i - 1] * nums[i - 1];
+      val ans = IntArray(nums.size)
+		var mul = 1
+        for (i in nums.indices) {
+            ans[i] = mul
+            mul*=nums[i]
         }
        
         var right = 1
-        for (i in n-1 downTo 0 ) {
-            result[i] *= right;
+        for (i in nums.indices.reversed()) {
+            ans[i] *= right;
             right *= nums[i];
         } 
-        return result 
+        return ans 
     }
 }
