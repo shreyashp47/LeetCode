bddd85c2df901e7c556a8fa4759382a71c5e6571
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2024-12-29 11:15:05 +0530

    Time: 0 ms (100.00%) | Memory: 36.3 MB (41.62%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.kt
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.kt
@@ -1,26 +1,22 @@
 class Solution {
     fun search(nums: IntArray, target: Int): Int {
-      var l = 0
-      var h = nums.size-1
-      
-      while(l<=h){
-          var mid = l + (h-l)/2
-          if(nums[mid] == target) return mid
-           if (nums[l] <= nums[mid]) {
- 			if (target >= nums[l] && target < nums[mid]) {
-                h = mid - 1
-            } else {
-                l = mid + 1
-            }
-          }else {
-              if(target > nums[mid] && target <= nums[h]) l = mid + 1 
-              else h = mid - 1 
-              
-          }
+     
+     var l = 0 
+     var h = nums.size-1
+     while(l<=h){
+         val mid = l+(h-l)/2
+         if(nums[mid]==target) return mid
+         
+         if(nums[l]<=nums[mid]){
+         if(target<nums[mid] && target>=nums[l]) h = mid-1
+         else l = mid + 1
+         }else {
+             if(target > nums[mid] && target <= nums[h]) l = mid + 1 
+             else h = mid-1 
           
-          
-      }
-      
-      return -1
+         }
+         
+     }
+        return -1
     }
 }
