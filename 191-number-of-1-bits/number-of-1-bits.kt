7d35936eeda178470fe60b0dc5bbe92a3fab44af
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2024-12-31 10:37:09 +0530

    Time: 1 ms (85.89%) | Memory: 34.7 MB (20.33%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.kt
+++ 191-number-of-1-bits/number-of-1-bits.kt
@@ -1,13 +1,13 @@
 class Solution {
     fun hammingWeight(n: Int): Int {
-         var count = 0
-         var num = n
-       while(num>0){
-            if(num % 2 == 1){
-                count++
-            }
-            num /= 2
+       var current = n
+        var bits = 0
+
+        while (current != 0) {
+            if (current % 2 == 1) bits++
+            current /= 2
         }
-         return count
+
+        return bits
     }
 }
