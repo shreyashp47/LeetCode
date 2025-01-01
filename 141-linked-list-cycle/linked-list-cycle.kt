c83f8db93016ca3ab28e66c77ceb8ea9b16caeb7
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2025-01-01 19:56:50 +0530

    Time: 183 ms (76.23%) | Memory: 39.2 MB (10.22%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.kt
+++ 141-linked-list-cycle/linked-list-cycle.kt
@@ -10,15 +10,17 @@
 
 class Solution {
     fun hasCycle(head: ListNode?): Boolean {
-        if(head == null || head.next == null) return false
-        var node = head
-        var map = HashMap<ListNode,Int>()
-        while(node!=null){
-            if(map.containsKey(node.next)) return true
+        var slow = head
+    var fast = head
 
-            map[node] = node.`val`
-            node = node.next
+    while (fast != null && fast.next != null) {
+        slow = slow?.next
+        fast = fast.next?.next
+
+        if (slow == fast) {
+            return true
         }
+    }
         return false
     }
 }
