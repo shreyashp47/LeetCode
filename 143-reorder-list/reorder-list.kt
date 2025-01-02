9ed8f350131c148a97d2cf8e98ee05f379ce4ff5
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2025-01-02 17:06:41 +0530

    Time: 3 ms (72.58%) | Memory: 40.8 MB (43.55%) - LeetSync

---
+++ 143-reorder-list/reorder-list.kt
@@ -0,0 +1,46 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun reorderList(head: ListNode?): Unit {
+       var slow = head
+       var fast = head
+
+       while(fast != null && fast.next != null){
+        slow = slow!!.next
+        fast = fast!!.next!!.next
+       }
+
+       var second = slow!!.next
+        slow.next = null
+       var prev: ListNode? = null
+
+       while(second!=null){
+        var next = second.next
+        second.next = prev 
+        prev = second 
+        second = next
+       }
+        
+        var first = head 
+         second = prev
+       while(second!=null){
+        var temp1 = first!!.next
+        var temp2 = second!!.next
+
+        first.next = second
+        second.next = temp1
+
+        first = temp1
+        second = temp2 
+
+       }
+    }
+ 
+}
