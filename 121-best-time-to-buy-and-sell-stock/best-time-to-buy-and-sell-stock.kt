e64270d027cb3e7d47ff0c12e612fe97f37fd75f
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2025-01-06 22:06:12 +0530

    Time: 3 ms (90.13%) | Memory: 56.9 MB (46.81%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.kt
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.kt
@@ -1,7 +1,7 @@
 class Solution {
     fun maxProfit(prices: IntArray): Int {
          if (prices.isEmpty()) return 0
-    var max = 0
+        var max = 0
         var minPrice = prices[0]
         for (price in prices) {
             val diff = price - minPrice
