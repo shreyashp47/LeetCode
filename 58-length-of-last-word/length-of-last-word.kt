91d344d2e99be65123ab804de86a596c1180d345
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2025-01-06 22:43:50 +0530

    Time: 0 ms (100.00%) | Memory: 35.6 MB (81.21%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.kt
+++ 58-length-of-last-word/length-of-last-word.kt
@@ -1,13 +1,18 @@
 class Solution {
     fun lengthOfLastWord(s: String): Int {
         
-        var str = s.trim()
+        var str = s
         var r = str.length-1 
+        var count = 0
+          while(r>=0 && str[r] == ' '  ){ 
+            r--
+          }
        
         while(r>=0 && str[r] != ' '  ){ 
+            count++
             r--
             
         }
-        return  str.length-1 - r
+        return  count
     }
 }
