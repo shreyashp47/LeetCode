db08edb94e7bc36b91adf43e9cbb84dcf1db642e
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2025-01-09 12:27:15 +0530

    Time: 13 ms (51.40%) | Memory: 38 MB (71.70%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.kt
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.kt
@@ -1,20 +1,22 @@
 class Solution {
     fun lengthOfLongestSubstring(s: String): Int {
-        var count = 0
-     var map = HashMap<Char,Int>() 
-     
-     var i = 0  
-     for(j in s.indices){
-         if (map.containsKey(s[j])) {
-            // Move i to the right of the last occurrence of s[j]
-            i = Math.max(map[s[j]]!! + 1, i)
+      var map = mutableSetOf<Char>()
+      var slow = 0 
+      var fast = 0 
+      var max = 0
+
+      while(fast < s.length){
+        if(!map.contains(s[fast])){
+            map.add(s[fast])
+            fast++
+            max = Math.max(max,map.size)
+        }else{
+            map.remove(s[slow])
+            slow++
         }
-        // Update the map with the latest index of s[j]
-        map[s[j]] = j
-        // Calculate the max length
-        count = Math.max(count, j - i + 1)
-     } 
-     
-     return count
+
+
+      }
+return max
     }
 }
