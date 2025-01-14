6004e525fe2b0f46a7375e9c5c262ca82be3e852
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2025-01-14 18:30:18 +0530

    Time: 208 ms (24.70%) | Memory: 38.5 MB (23.48%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.kt
@@ -0,0 +1,24 @@
+class Solution {
+    fun removeDuplicates(nums: IntArray): Int {
+      if (nums.size <= 2) return nums.size
+  
+     var num = nums[0]  // nums.length >= 1
+     var count = 0
+     var j = 0
+
+        for (i in nums.indices) {
+            if(num == nums[i]) {
+                count++ 
+                if(count<3) nums[j++] = num
+            }else{
+                num = nums[i]
+                nums[j++] = num
+                count = 1
+                
+            }
+        
+        }
+     
+     return j
+    }
+}
