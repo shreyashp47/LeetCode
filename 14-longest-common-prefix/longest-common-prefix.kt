b208d263564406026272e874dc65a1292bc6ac96
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2025-01-15 19:46:23 +0530

    Time: 2 ms (81.68%) | Memory: 35.7 MB (91.35%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.kt
+++ 14-longest-common-prefix/longest-common-prefix.kt
@@ -6,19 +6,14 @@ fun longestCommonPrefix(strs: Array<String>): String {
     	val first = strs[0]
 
       	for (i in first.indices) {
-            var currentChar = first[i]
-            
-            println("currentChar "+currentChar)
+            var currentChar = first[i] 
             for(j in 1 until strs.size ){
                  if(i >= strs[j].length  ||   currentChar != strs[j][i])
                  return pref
-   
-                println("  - "+strs[j])
+    
             }
             pref = pref + currentChar
-        }
-        
-        println("ans $pref")
+        } 
         
         return pref
         
