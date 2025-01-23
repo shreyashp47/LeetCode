1b7a4fe6c0f86c7a25ff762016cb1aed59fdb7d0
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2025-01-23 22:20:24 +0530

    Time: 13 ms (76.50%) | Memory: 38 MB (75.50%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.kt
+++ 205-isomorphic-strings/isomorphic-strings.kt
@@ -3,11 +3,9 @@ class Solution {
         var map = HashMap<Char,Char>()
 
         for(i in 0..s.length-1){
-            if(map.containsKey(s[i])){
+            if(map.containsKey(s[i]) || map.containsValue(t[i])){
                 if(map.getOrDefault(s[i],"") != t[i]) return false
                
-            }else if(map.containsValue(t[i])){
-                if(map.getOrDefault(s[i],"") != t[i]) return false
             }else {
                 map[s[i]] = t[i]
             }
