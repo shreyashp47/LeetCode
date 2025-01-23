45e0f54752de7ca39161f703de977f694c85a72d
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2025-01-23 22:02:53 +0530

    Time: 7 ms (34.90%) | Memory: 54.2 MB (26.49%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.kt
+++ 11-container-with-most-water/container-with-most-water.kt
@@ -1,19 +1,15 @@
 class Solution {
     fun maxArea(height: IntArray): Int {
-        var left = 0 
-        var right = height.size - 1
         var ans = 0
+        var left = 0
+        var right = height.size - 1
+
         while(left<right){
-            val minHeight = Math.min(height[left],height[right])
-            val mostWater = (right - left ) * minHeight
-
-            
-            ans = Math.max(ans,mostWater)
-            if(height[left]>height[right]) right--
-            else left++
-
-        } 
-
+            var min = Math.min(height[left], height[right])
+            ans = Math.max(ans, min * (right-left))
+            if(height[left] > height[right]) right--
+            else left ++
+        }
         return ans
     }
 }
