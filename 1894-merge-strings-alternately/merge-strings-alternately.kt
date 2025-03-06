2f484103cf8f0978035b5bb82ea72654ea3e6e01
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2025-03-06 09:42:27 +0530

    Time: 149 ms (63.10%) | Memory: 37.3 MB (26.94%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.kt
+++ 1894-merge-strings-alternately/merge-strings-alternately.kt
@@ -1,21 +1,13 @@
 class Solution {
     fun mergeAlternately(word1: String, word2: String): String {
-        
-    
         var w1 = 0
         var w2 = 0 
-        var ans = ""
-        var switch = true
+        var ans = "" 
         while(w1 < word1.length || w2< word2.length){
-            if(switch && w1 < word1.length){
-                ans = ans + word1[w1++]
-            }else if(w2 < word2.length ){
-                ans = ans+ word2[w2++]
-            }else {
-                  ans = ans + word1[w1++]
-            }
-            switch =  !switch 
-
+            if(w1 < word1.length)
+            ans = ans + word1[w1++]
+            if (w2< word2.length)
+            ans = ans+ word2[w2++]
         }
 
         return ans
