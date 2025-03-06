f930900215627be2f60ce6229bcc829db47fd430
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2025-03-06 09:46:47 +0530

    Time: 157 ms (43.41%) | Memory: 35 MB (92.67%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.kt
+++ 1894-merge-strings-alternately/merge-strings-alternately.kt
@@ -4,12 +4,12 @@ class Solution {
         var w2 = 0 
         
         var ans = StringBuilder()
-        while(w1 < word1.length || w2< word2.length){
-            if(w1 < word1.length)
+        while(w1 < word1.length && w2< word2.length){
             ans.append(word1[w1++])
-            if (w2< word2.length)
             ans.append(word2[w2++])
         }
+        while(w1 < word1.length)  ans.append(word1[w1++])
+         while(w2 < word2.length)  ans.append(word2[w2++])
 
         return ans.toString()
 
