6e1e611dc1234f6fc503d43f6cc2d57ef98e144e
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2025-03-06 09:47:31 +0530

    Time: 148 ms (65.49%) | Memory: 35.6 MB (53.62%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.kt
+++ 1894-merge-strings-alternately/merge-strings-alternately.kt
@@ -1,17 +1,26 @@
 class Solution {
     fun mergeAlternately(word1: String, word2: String): String {
-        var w1 = 0
-        var w2 = 0 
-        
-        var ans = StringBuilder()
-        while(w1 < word1.length && w2< word2.length){
-            ans.append(word1[w1++])
-            ans.append(word2[w2++])
+        var i = 0
+        var j = 0
+        val strBuilder = StringBuilder()
+
+        while(i < word1.length && j < word2.length) {
+            strBuilder.append(word1[i])
+            strBuilder.append(word2[j])
+            i++
+            j++
         }
-        while(w1 < word1.length)  ans.append(word1[w1++])
-         while(w2 < word2.length)  ans.append(word2[w2++])
 
-        return ans.toString()
+        while (i < word1.length) {
+            strBuilder.append(word1[i])
+            i++
+        }
 
+        while (j < word2.length) {
+            strBuilder.append(word2[j])
+            j++
+        }
+
+        return strBuilder.toString()
     }
 }
