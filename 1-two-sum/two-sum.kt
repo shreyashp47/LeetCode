09f10bc8b97e6429d0be70f61aa1edc0598e1d56
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2025-09-01 16:58:07 +0530

    Time: 3 ms (67.54%) | Memory: 47.8 MB (63.51%) - LeetSync

--- 1-two-sum/two-sum.kt
+++ 1-two-sum/two-sum.kt
@@ -1,14 +1,12 @@
 class Solution {
-    fun twoSum(nums: IntArray, target: Int): IntArray {
-        var map = HashMap<Int,Int>()
-
-        for(i in 0..nums.size){
-            if(map.containsKey(target - nums[i])){
-              return  intArrayOf(i , map[target-nums[i]]!!)
-            }
-            map[nums[i]] = i 
-        }
+      fun twoSum(nums: IntArray, target: Int): IntArray {
+      var list = mutableMapOf<Int,Int>()
+      for(i in nums.indices){
+          var diff = target - nums[i]
+          if(list.contains(diff)) 
+          return intArrayOf(list[diff]!!,i)
+          list[nums[i]] =  i
+      }
         return intArrayOf()
-        
     }
 }
