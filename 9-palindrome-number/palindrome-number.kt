a4af3b2ddb1ff868d8800d0048fe07ab8a5af101
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2025-11-13 09:32:55 +0530

    Time: 20 ms (9.03%) | Memory: 46.7 MB (14.35%) - LeetSync

--- 9-palindrome-number/palindrome-number.kt
+++ 9-palindrome-number/palindrome-number.kt
@@ -1,21 +1,8 @@
 class Solution {
-    fun isPalindrome(x: Int): Boolean {
-        var rev =  reverse(x)
-        if(rev == x && x>=0) return true
+fun isPalindrome(x: Int): Boolean {
+        var rev : String = ""+x
+    	rev = rev.reversed()
+        if(rev == x.toString()) return true
    			else return false
-        
-    }
-fun reverse(x: Int): Int {
-        var ans = 0
-        var re = x
-        while(re!=0){
-            val rem = re%10
-            re = re/10
-            ans = ans*10+rem
-            
-        }
-        return ans
-        
-    }
-
+}
 }
