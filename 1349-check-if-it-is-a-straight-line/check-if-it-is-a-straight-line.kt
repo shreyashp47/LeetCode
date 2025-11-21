19d0d0abac7142fdabcda0d0af9b3c2c603c7aa5
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2025-11-21 08:52:54 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (45.45%) - LeetSync

--- 1349-check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.kt
+++ 1349-check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.kt
@@ -1,27 +1,19 @@
 class Solution {
     fun checkStraightLine(coordinates: Array<IntArray>): Boolean {
-   
-        val x1 = coordinates[0][0]
-        val y1 = coordinates[0][1]
-        val x2 = coordinates[1][0]
-        val y2 = coordinates[1][1]
-        
-        val dx = x2 - x1
-        val sp = y2 - y1
-        
-       
-        for(i in 2 until coordinates.size){
-              
+        val x0 = coordinates[0][0]
+        val y0 = coordinates[0][1]
+        val x1 = coordinates[1][0]
+        val y1 = coordinates[1][1]
+
+        val difX = x1 - x0
+        val difY = y1 - y0 
+
+        for(i in 2..coordinates.size-1){
             val x = coordinates[i][0]
             val y = coordinates[i][1]
-            
-          
-             if ((y - y1) * dx != (x - x1) * sp) {
-                return false
-            }
-             
+            if(difX * (y - y0) != difY * (x - x0)) return false
         }
         return true
-        
+
     }
 }
