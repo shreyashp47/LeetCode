79e8ec509913d7bad414cc3334ebfc254e2432d9
Shreyash Pattewar <34227385+shreyashp47@users.noreply.github.com>
2025-11-25 09:03:47 +0530

    Time: 1 ms (50.68%) | Memory: 40.6 MB (90.41%) - LeetSync

--- 231-power-of-two/power-of-two.kt
+++ 231-power-of-two/power-of-two.kt
@@ -1,10 +1,13 @@
 class Solution {
     fun isPowerOfTwo(n: Int): Boolean {
-   for (i in 0..30) { // Adjusted the range
-        val num = 1 shl i // 2^i is equivalent to shifting 1 by i bits
-        if (num == n)
-            return true
-    }
-    return false
+        var num = n
+        if(num == 0) return false 
+        while(num%2 == 0){
+            num = num/2
+            
+        }
+        if(num == 1) return true 
+        else 
+            return false
     }
 }
